deb4b7ca3872bef2147b977376ce9556ca96ad37
chanderpal <pal.3@iitj.ac.in>
2023-08-07 00:48:04 +0530

    first commit

---
+++ app/schema.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel,EmailStr
+from datetime import datetime
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserCreate(UserBase):
+    pass
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    username: str
+    created_at: datetime
